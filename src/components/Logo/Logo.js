b4539f85f63027f88aa7a69006e2bf0b9ce81ebb
Achi <achiugulava@gmail.com>
2022-05-08 12:18:18 +0400

    first commit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,9 +6,9 @@ import './Logo.css';
 const Logo = () => {
   return (
     <div className='ml2 mt2'>
-      <Tilt className="Tilt br-100 shadow-4" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+      <Tilt className="Tilt br-100 shadow-4" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
         <div className="Tilt-inner pa3">
-          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+          <img  alt='logo' src={brain}/>
         </div>
       </Tilt>
     </div>
